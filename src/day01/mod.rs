05e08e7db731592efe1ded2d449b17d8ab42d29f
Daniel Danner <Daniel.Danner@methodpark.de>
2021-12-01 19:38:37 +0100

    Add solution for day 1 part 2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -17,6 +17,14 @@ pub fn solve_part1(input: &Vec<u32>) -> Result<usize, ParseIntError> {
         .count())
 }
 
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &Vec<u32>) -> Result<usize, ParseIntError> {
+    Ok(input
+        .windows(4)
+        .filter(|v| v[3] > v[0])
+        .count())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -27,6 +35,16 @@ mod test {
         let expected_output = 7;
 
         let output = solve_part1(&input).unwrap();
+
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    pub fn test_solve_part2() {
+        let input = vec![ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let expected_output = 5;
+
+        let output = solve_part2(&input).unwrap();
 
         assert_eq!(output, expected_output);
     }
